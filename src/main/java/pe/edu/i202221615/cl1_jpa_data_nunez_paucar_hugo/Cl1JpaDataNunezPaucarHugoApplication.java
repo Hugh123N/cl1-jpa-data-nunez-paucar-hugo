10930f5494262c18309f0ed2c8168630886a498e
Hugh123N <hugonunezpaucar123@hotmail.com>
2024-11-25 10:30:56 -0500

    creacion de proyecto

---
+++ src/main/java/pe/edu/i202221615/cl1_jpa_data_nunez_paucar_hugo/Cl1JpaDataNunezPaucarHugoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202221615.cl1_jpa_data_nunez_paucar_hugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataNunezPaucarHugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataNunezPaucarHugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202221615/cl1_jpa_data_nunez_paucar_hugo/Cl1JpaDataNunezPaucarHugoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202221615.cl1_jpa_data_nunez_paucar_hugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataNunezPaucarHugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
